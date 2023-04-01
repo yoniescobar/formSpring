b0b47d2a708a80b53d8157112480c763d826aa70
yoniedilzar <yoniedilzar@gmail.com>
2023-04-01 11:58:23 -0600

    formulario mejorado

--- src/main/java/com/colegiox/colegio/EstudianteController.java
+++ src/main/java/com/colegiox/colegio/EstudianteController.java
@@ -3,8 +3,9 @@ package com.colegiox.colegio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,14 +13,29 @@ import java.util.List;
 @Controller
 public class EstudianteController {
 
-    @GetMapping("/registro")
+    @GetMapping({"index","/"}) // localhost:8080/index
+    public String index(){
+
+        return "index";
+    }
+
+    @GetMapping("/registro") // localhost:8080/registro  Get
     public String ShowForm(Model model){
         Estudiante estudiante = new Estudiante();
         model.addAttribute("estudiante", estudiante);
+
+        List<String> lisCarreras = Arrays.asList("Desarrollador ", "Diseñador", "Administrador", "Contador");
+        //Arrays.asList: Convierte un arreglo en una lista de elementos de tipo String (List<String>)
+        model.addAttribute("lisCarreras", lisCarreras);
 
 
         return "registro";
 
     }
 
+    @PostMapping("/registro")
+    public String submitForm(@ModelAttribute Estudiante estudiante, Model model){
+        model.addAttribute("estudiante", estudiante);
+        return "registro_success";
+    }
 }
